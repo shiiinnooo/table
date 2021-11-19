c9fb406cbc80cb878ca3f99261968c5eeacabc8b
Shino <smile0919a@gmail.com>
2021-11-19 15:51:21 +0800

    Perf(About頁面) 加入RWD

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
   <div class="bg-light">
     <div class="container pt-3 pb-5">
       <div class="content row">
-        <div class="content-text col-6 d-flex align-items-center justify-content-center">
-          <p class="lh-lg fs-6 text-center">
+        <div class="content-text col-md-6 d-flex align-items-center justify-content-center">
+          <p class="lh-lg fs-6 text-center py-4">
             現代人忙於工作、生活，<br>
             常常快速的解決一餐，<br>
             你有多久沒有坐在餐桌前好好吃頓飯了呢？<br>
@@ -13,8 +13,8 @@
             用心感受周遭的每個角落：）<br>
           </p>
         </div>
-        <div class="content-img col-6">
-          <img src="https://images.pexels.com/photos/6207518/pexels-photo-6207518.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" height="700">
+        <div class="content-img col-md-6">
+          <img class="w-100" src="https://images.pexels.com/photos/6207518/pexels-photo-6207518.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" height="700">
         </div>
       </div>
     </div>
