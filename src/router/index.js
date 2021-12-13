4401ff69a024719f9656825b6c3d93860f55f26e
Shino <smile0919a@gmail.com>
2021-12-13 19:55:23 +0800

    change router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -79,7 +79,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   linkActiveClass: 'active',
   scrollBehavior(to, from) {
